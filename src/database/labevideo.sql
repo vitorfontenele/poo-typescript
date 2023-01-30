c34b6697000bcea9d20dbe5286a2b1d6b6cd6d15
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-01-30 16:51:42 -0300

    Update

--- src/database/labevideo.sql
+++ src/database/labevideo.sql
@@ -8,13 +8,13 @@ CREATE TABLE videos (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     title TEXT NOT NULL,
     duration REAL NOT NULL,
-    uploaded_at TEXT DEFAULT (DATETIME()) NOT NULL
+    uploaded_at TEXT NOT NULL
 );
 
 -- query c
-INSERT INTO videos(id, title, duration) VALUES
-    ("v001", "Git installation tutorial", 600),
-    ("v002", "10 use cases of ChatGPT for devs", 660),
-    ("v003", "Figma crash course", 3600),
-    ("v004", "Understand useEffect in 10 minutes", 600),
-    ("v005", "Classes and objects in Python", 900);
+INSERT INTO videos(id, title, duration, uploaded_at) VALUES
+    ("v001", "Git installation tutorial", 600, "2023-01-30T19:47:28.936Z"),
+    ("v002", "10 use cases of ChatGPT for devs", 660, "2023-01-30T19:47:28.936Z"),
+    ("v003", "Figma crash course", 3600, "2023-01-30T19:47:28.936Z"),
+    ("v004", "Understand useEffect in 10 minutes", 600, "2023-01-30T19:47:28.936Z"),
+    ("v005", "Classes and objects in Python", 900, "2023-01-30T19:47:28.936Z");
